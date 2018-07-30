917a30b965b341e898670c0ba68871c8fb5ef89f
JasoneDDev <41653034+JasoneDDev@users.noreply.github.com>
2018-07-30 10:01:35 -0600

    Delete HitColliderNode.js
    useless script

--- HitColliderNode.js
+++
@@ -1,7 +0,0 @@
-#pragma strict
-
-//function OnDrawGizmos()
-//{
-//	Gizmos.color = Color(.4,.7,1,0.2);
-//	//Gizmos.DrawCube((transform.position + GetComponent(BoxCollider).center), GetComponent(BoxCollider).size);
-//}
